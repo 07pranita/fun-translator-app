8482d86535e01e9dc8bddf2e9f3476f293822824
Pranita <pfulsundar8@gmail.com>
2020-12-05 13:13:24 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+
+var btnTranslate = document.querySelector('.btn-translate');
+var inputText = document.querySelector('#txt-input');
+var outputText = document.querySelector('.output');
+
+var serverURL1 = "https://api.funtranslations.com/translate/minion.json";
+var serverURL2 = "https://api.funtranslations.com/translate/minion.json";
+var serverURL3 = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text= " + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured " + error);
+    alert("Something wrong with server. Try again later.");
+}
+
+function clickHandler(){
+    var txtInput = inputText.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(txtInput))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText;  //output
+    })
+    .catch(errorHandler)
+    // outputText.innerText = "abs " + inputText.value;
+}
+
+btnTranslate.addEventListener("click", clickHandler);
